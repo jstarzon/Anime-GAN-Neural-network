06485f47f78a998a1cf6b5be423590de0b97008a
Juliusz <117957835+jstarzon@users.noreply.github.com>
2023-02-03 01:51:55 +0100

    add models folder

--- generator.py
+++ generator.py
@@ -1,8 +1,8 @@
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import numpy as np
-
-
+import os
+model_dir = 'models'
 def generate_images(generator):
     noise = np.random.normal(0, 1, (1, 100))
     generated_images = generator.predict(noise)
@@ -10,5 +10,5 @@ def generate_images(generator):
     plt.axis("off")
     plt.savefig("generated.jpg")
     plt.show()
-gan = load_model('generator_model.h5')
+gan = load_model(os.path.join(model_dir, 'generator_model.h5'))
 generate_images(gan)
